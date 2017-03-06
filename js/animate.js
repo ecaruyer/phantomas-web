2585634d1a31b1456712c15b0f1a77008afa9a11
Guillem Gonzalez Vela <guillem.gonzalez-vela@inria.fr>
2017-03-06 12:06:34 +0100

    Added css.main so to set full size canvas, disable margins and avoid displacement bars.

--- js/animate.js
+++ js/animate.js
@@ -46,7 +46,7 @@ function init() {
   }
 
   // Load phantom and add it in the scene
-  phantom = loadPhantom("examples/3Dfanning_13bundles.txt");
+  phantom = loadPhantom("examples/fibers.txt");
   phantom.highlightopacity = .2;
   phantom.addToScene(scene);
   camera.position.z = phantom.radius()*1.5;
